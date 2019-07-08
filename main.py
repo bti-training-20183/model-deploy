d81d8ffca39c01fd3e2fc5a4e69b2fbf5fdd7ffe
Le Cong Hau <anhlahau.hl@gmail.com>
2019-07-08 17:32:02 +0700

    Simple upload file to minio

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import config
 import os
+import io
 import sys
+from minio import Minio
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+                         BucketAlreadyExists)
 sys.path.append(os.getcwd())
 from utils.message_handler import MessageHandler
 
@@ -8,9 +12,41 @@ from utils.message_handler import MessageHandler
 
 def callback(channel, method, properties, body):
     print(f'[x] Received {body} from {properties}')
-    MessageHdlr.sendMessage(
-        'from_deployer', 'Dummy message from Deployer')
+    bucketName = "file-storage"
+    minioClient = Minio(
+                  'play.min.io:9000', # For online storage
+                  access_key='Q3AM3UQ867SPQQA43P2F',
+                  secret_key='zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+                  secure=True)
+    
+    # Make a bucket with the make_bucket API call.
+    try:
+        minioClient.make_bucket(bucketName, location="us-east-1")
+    except BucketAlreadyOwnedByYou as err:
+        print('BucketAlreadyOwnedByYou')
+        pass
+    except BucketAlreadyExists as err:
+        print('BucketAlreadyExists')
+        pass
+    except ResponseError as err:
+        print('ResponseError')
+        raise
 
+    # Put an object with contents .
+    try:
+        # create file object from message body
+        f = io.BytesIO(body)
+        fileName = properties.headers['filename']
+        minioClient.put_object(bucketName, fileName, f,len(body) )
+        file_uri = f'https://play.min.io:9000/{bucketName}/{fileName}'
+        MessageHdlr.sendMessage('from_deployer', file_uri)
+    except KeyError as err:
+        MessageHdlr.sendMessage('from_deployer',"Filename not found")
+        print("Filename not found")
+    except ResponseError as err:
+        MessageHdlr.sendMessage('from_deployer',err)
+        print(err)
+    
 
 class Deployer:
     def __init__(self):
