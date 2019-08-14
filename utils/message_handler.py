b2f0a4cbca7053d357cbb777050566e9cbd84d26
Le Cong Hau <anhlahau.hl@gmail.com>
2019-08-14 13:33:18 +0700

    Fix Message Handler

--- utils/message_handler.py
+++ utils/message_handler.py
@@ -30,32 +30,64 @@ class MessageHandler:
             self.channel.queue_declare('from_deployer')
     
     def _connect(self):
-        self.connection = pika.BlockingConnection(
-                pika.ConnectionParameters(config.RABBITMQ_CONNECTION))
+        credentials = pika.PlainCredentials('guest', 'guest')
+        parameters =  pika.ConnectionParameters(config.RABBITMQ_CONNECTION, credentials=credentials, heartbeat=5)
+        self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
+        self.channel.basic_qos(prefetch_count=1)
+        self.threads = []
+        
     
     def _publish(self, queue, body):
-        self.channel.basic_publish(
+        # Should use separate connection to publish and comsume
+        publisher = MessageHandler(config.RABBITMQ_CONNECTION)
+        publisher.channel.basic_publish(
                 exchange='', routing_key=queue, body=body)
         print(f" [x] Sent {body} to queue: {queue}")
+        print("Close publisher")
+        publisher.close()
     
     def sendMessage(self, queue, body):
         try:
             self._publish(queue, body)
-        except Exception:
-            print('\n\nReconnecting to queue\n\n-------------------------------')
-            self._connect()
-            self._publish(queue, body)
-            
-    def on_request(self, channel, method, properties, body, callback):
-        callback_thread = threading.Thread(target=callback, args=(channel, method, properties, body))
-        callback_thread.start()
+        except Exception as e:
+            print("Stop with Error:", e)
 
     def consumeMessage(self, queue, callback):
-        self.channel.basic_consume(
-            queue=queue, on_message_callback=partial(self.on_request, callback=callback), auto_ack=True)
+        on_message_callback = partial(self.on_message, args=(self.connection, self.threads, callback))
+        self.channel.basic_consume(queue=queue,on_message_callback=on_message_callback)
         print(f' [*] Waiting for messages from {queue}. To exit press CTRL+C')
-        self.channel.start_consuming()
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.channel.stop_consuming()
+        
+        # Wait for all to complete
+        for thread in self.threads:
+            thread.join()
+        
+        self.connection.close()
+
+    def ack_message(self, channel, delivery_tag):
+        if channel.is_open:
+            channel.basic_ack(delivery_tag)
+
+    def on_message(self, channel, method_frame, header_frame, body, args):
+        (connection, threads, callback) = args
+        delivery_tag = method_frame.delivery_tag
+        t = threading.Thread(target=self.do_work, args=(connection, channel, delivery_tag, body, method_frame, header_frame, callback))
+        t.start()
+        threads.append(t)
+
+    def do_work(self, connection, channel, delivery_tag, body,  method_frame, header_frame, callback):
+        thread_id = threading.get_ident()
+        fmt1 = 'Thread id: {} Delivery tag: {} Message body: {}'
+        
+        # Run callback function to train model, and ...
+        callback(channel, method_frame, header_frame, body)
+        
+        cb = partial(self.ack_message, channel, delivery_tag)
+        connection.add_callback_threadsafe(cb)
 
     def close(self):
         self.connection.close()
